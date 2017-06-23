ca5fbe566aca84b40160a1b5883af3a141fca61c
kevin <kevin.e@qq.com>
2017-06-23 14:00:30 +0800

    teble

--- src/components/TableModule.vue
+++ src/components/TableModule.vue
@@ -1,20 +1,69 @@
 <template>
 <div>
-  <div class="column-link"><a class="column" @click="dialogVisible">自定义列</a> &nbsp; <a class="download">下载</a></div>
+  <div>
+  <el-row>
+    <el-col :span="24">
+        <div style="float:left;">
+          <span>搜索数据 : </span>
+            <div style="margin-top: 15px;width:350px;display:inline-block">
+              <el-input placeholder="请输入内容" v-model="searchInput">
+                <el-button slot="append" icon="search"></el-button>
+              </el-input>
+            </div>
+        <a @click="showAdvancedSearch" v-if="!AdvancedSearchVisible" style="margin-left:10px;color:#108ee9;cursor:pointer;">高级搜索</a>
+        </div>
+    </el-col>
+  </el-row>
+
+  <el-row v-if="AdvancedSearchVisible">
+  <el-col :span="24">
+    <div style="float:left;padding-top:15px;margin-top:15px;border-top: 1px solid #cccccc;">
+      <span>高级搜索 : </span>
+      <el-select @change="handleFristColumnChange" v-model="searchValue1" placeholder="请选择列">
+        <el-option
+          v-for="item in tableCol"
+          :key="item.key"
+          :label="item.label"
+          :value="item.key">
+        </el-option>
+      </el-select>
+      <el-input v-model="input" style="width:50px"></el-input> -
+      <el-input v-model="input" style="width:50px"></el-input>
+      <el-select v-model="relationalValue" style="width:60px;margin:0 15px">
+        <el-option
+          v-for="item in relationalOptions"
+          :key="item.label"
+          :label="item.label"
+          :value="item.label">
+        </el-option>
+      </el-select>
+      <el-select v-model="searchValue2" placeholder="请选择列">
+        <el-option
+          v-for="item in tableCol"
+          :key="item.key"
+          :label="item.label"
+          :value="item.key">
+        </el-option>
+      </el-select>
+      <el-input v-model="input" style="width:50px"></el-input> -
+      <el-input v-model="input" style="width:50px"></el-input>
+    <el-button style="margin:0 20px" type="primary" icon="search">搜索</el-button>
+    <a @click="hiddenAdvancedSearch" style="margin-left:10px;color:#108ee9;cursor:pointer;">收起</a>    
+    </div>
+    </el-col>
+  </el-row>
+  </div>
+
+  <div class="column-link"><a @click="dialogVisible">自定义列</a></div>
   <el-dialog title="自定义表格列" :visible.sync="columnVisible">
   <el-alert
-    title="至少选3个，最多选5个指标到展示列表，您也可以随时再调整。"
+    class="tips"
+    title="5个指标能更方便展示列表，您也可以随时再调整。"
     type="info"
     show-icon>
   </el-alert>
-    <el-checkbox-group 
-      v-model="checkedColumn"
-      :min="3"
-      :max="5">
-      <el-checkbox v-for="item in allColumn" :label="item.label" :key="item.label">{{item.label}}</el-checkbox>
-    </el-checkbox-group>
+  <el-checkbox class="choice-col" v-for="item in tableCol"  :key="item.key" :label="item.label" v-model="item.visible" >{{item.label}}</el-checkbox>
     <div slot="footer" class="dialog-footer">
-      <el-button @click="columnVisible = false">取 消</el-button>
       <el-button type="primary" @click="columnSubmit">确 定</el-button>
     </div>
   </el-dialog>
@@ -24,9 +73,11 @@
     :data="tableData3"
     align='left'
     border
+    stripe
     tooltip-effect="dark"
     style="width: 100%"
     highlight-current-row
+    @sort-change="handleSortChange"
     @selection-change="handleSelectionChange">
 
     <el-table-column
@@ -35,16 +86,26 @@
     </el-table-column>
     <el-table-column
       v-for="col in tableCol"
+      v-bind:key="col.key"
       v-if="col.visible"
+      v-bind:sortable="col.sortable"
+      v-bind:filters="col.filters"
+      @filter-method="filterTag"
       v-bind:prop="col.prop"
       v-bind:label="col.label" >
     </el-table-column>
      
   </el-table>
-  <div style="margin-top: 20px">
-    <el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button>
-    <el-button @click="toggleSelection()">取消选择</el-button>
-  </div>
+  <el-pagination
+      class="pagination"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+      :page-sizes="[2, 50, 200, 500, 1000]"
+      :page-size="10"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
   </div>
 </template>
 
@@ -73,79 +134,103 @@
           address: '上海市普陀区金沙江路 1518 弄',
           age: 25
         }],
-        tableCol: [{key: 'date', prop: 'date', label: '日期', visible: true},
-                  {key: 'name', prop: 'name', label: '姓名', visible: true},
-                  {key: 'address', prop: 'sex', label: '性别', visible: true},
-                  {key: 'age', prop: 'age', label: '年龄', visible: false},
-                  {key: 'address', prop: 'address', label: '地址', visible: true},
+        // visible是否可见、sortable是否可排序、filters筛选数据源、search是否可搜索
+        // type 日期 date 时间 time  2017-06-23 11:39:08 字符串 string  数字 number  枚举 enun
+        tableCol: [{key: 'date', prop: 'date', label: '日期', visible: true, sortable: true, type: 'date'},
+                  {key: 'name', prop: 'name', label: '姓名', visible: true, search: true},
+                  {key: 'sex', prop: 'sex', label: '性别', visible: true, sortable: true},
+                  {key: 'age', prop: 'age', label: '年龄', visible: false, search: true, type: 'number'},
+                  {key: 'address', prop: 'address', label: '地址', visible: true, type: 'string'},
                   {key: 'nationality', prop: 'nationality', label: '籍贯', visible: false},
-                  {key: 'phone', prop: 'phone', label: '电话', visible: false},
+                  {key: 'phone', prop: 'phone', label: '电话', visible: false, sortable: true},
                   {key: 'email', prop: 'email', label: '邮箱', visible: false},
                   {key: 'conent', prop: 'conent', label: '紧急联系人', visible: false},
-                  {key: 'make', prop: 'make', label: '操作', visible: false}
+                  {key: 'time', prop: 'time', label: '时间', visible: true, search: true, type: 'time'},
+                  {key: 'make', prop: 'make', label: '操作', visible: false, filters: []},
+                  {key: 'state', prop: 'state', label: '状态', visible: false, type: 'enum', filters: [{text: '家', value: '家'}, {text: '公司', value: '公司'}]}
         ],
-        // 全部的列
-        allColumn: [],
-        // 显示中的列
-        checkedColumn: [],
+        // 搜索框
+        searchInput: '',
+        // 表格多选，选中的行
         multipleSelection: [],
-        columnVisible: false
+        columnVisible: false,
+        total: 5,
+        currentPage: 1,
+        AdvancedSearchVisible: false,
+        searchValue1: '',
+        searchValue2: '',
+        // 高级搜索与或关系选择器
+        relationalOptions: [{
+          value: 'and',
+          label: '与'
+        }, {
+          value: 'or',
+          label: '或'
+        }],
+        // 高级搜索与或关系选择器
+        relationalValue: ''
       }
     },
 
     methods: {
+      // 高级搜索选项一下拉列表框
+      handleFristColumnChange (selectItem) {
+        console.log(selectItem)
+      },
+      showAdvancedSearch () {
+        this.AdvancedSearchVisible = true
+      },
+      hiddenAdvancedSearch () {
+        this.AdvancedSearchVisible = false
+      },
       dialogVisible () {
-        if (this.allColumn.length === 0) {
-          for (let i = 0; i < this.tableCol.length; i++) {
-            this.allColumn.push({label: this.tableCol[i].label, visible: this.tableCol[i].visible})
-            if (this.tableCol[i].visible) {
-              this.checkedColumn.push({label: this.tableCol[i].label, visible: this.tableCol[i].visible})
-            }
-          }
-        }
         this.columnVisible = true
       },
       columnSubmit () {
-        for (let i = 0; i < this.checkedColumn.length; i++) {
-          for (let j = 0; j < this.tableCol.length; j++) {
-            if (this.tableCol[j].label === this.checkedColumn[i]) {
-              this.tableCol[j].visible = true
-              continue
-            } else {
-              this.tableCol[j].visible = false
-              continue
-            }
-          }
-        }
         this.columnVisible = false
       },
-      toggleSelection (rows) {
-        if (rows) {
-          rows.forEach(row => {
-            this.$refs.multipleTable.toggleRowSelection(row)
-          })
-        } else {
-          this.$refs.multipleTable.clearSelection()
-        }
-      },
+      // 表格多选响应
       handleSelectionChange (val) {
         this.multipleSelection = val
+      },
+      // 排序
+      handleSortChange (val) {
+        console.log(val)
+      },
+      // 筛选
+      filterTag (value, row) {
+        console.log(value, row)
+      },
+      // 分页
+      handleSizeChange (value) {
+        console.log(value)
+      },
+      // 当前页
+      handleCurrentChange (value) {
+        console.log(value)
       }
     }
   }
 </script>
 
 <style>
-.column,.download{
-  color:#108ee9;
-  cursor:pointer;
+
+.choice-col{
+  margin: 10px 20px;
 }
-.download{
-  padding-right: 20px;
-  background: url('../assets/download.png') no-repeat  center right;
+.tips{
+  margin: 10px;
 }
 .column-link{
+  color:#108ee9;
+  cursor:pointer;
   float: right;
-  margin: 20px;
+  padding-right: 20px;
+  margin:0 20px 5px;
+  background: url('../assets/ustomize.png') no-repeat  center right;
+}
+.pagination{
+  float: right;
+  padding:10px 20px;    
 }
 </style>
--- src/demo.vue
+++ src/demo.vue
@@ -1,70 +1,78 @@
-<!-- Table -->
-<el-button type="text" @click="dialogTableVisible = true">打开嵌套表格的 Dialog</el-button>
-
-<el-dialog title="收货地址" :visible.sync="dialogTableVisible">
-  <el-table :data="gridData">
-    <el-table-column property="date" label="日期" width="150"></el-table-column>
-    <el-table-column property="name" label="姓名" width="200"></el-table-column>
-    <el-table-column property="address" label="地址"></el-table-column>
+<template>
+  <el-table
+    :data="tableData"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      sortable
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址"
+      :formatter="formatter">
+    </el-table-column>
+    <el-table-column
+      prop="tag"
+      label="标签"
+      width="100"
+      :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+      :filter-method="filterTag"
+      filter-placement="bottom-end">
+      <template scope="scope">
+        <el-tag
+          :type="scope.row.tag === '家' ? 'primary' : 'success'"
+          close-transition>{{scope.row.tag}}</el-tag>
+      </template>
+    </el-table-column>
   </el-table>
-</el-dialog>
-
-<!-- Form -->
-<el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
-
-<el-dialog title="收货地址" :visible.sync="dialogFormVisible">
-  <el-form :model="form">
-    <el-form-item label="活动名称" :label-width="formLabelWidth">
-      <el-input v-model="form.name" auto-complete="off"></el-input>
-    </el-form-item>
-    <el-form-item label="活动区域" :label-width="formLabelWidth">
-      <el-select v-model="form.region" placeholder="请选择活动区域">
-        <el-option label="区域一" value="shanghai"></el-option>
-        <el-option label="区域二" value="beijing"></el-option>
-      </el-select>
-    </el-form-item>
-  </el-form>
-  <div slot="footer" class="dialog-footer">
-    <el-button @click="dialogFormVisible = false">取 消</el-button>
-    <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
-  </div>
-</el-dialog>
+</template>
 
 <script>
   export default {
     data() {
       return {
-        gridData: [{
+        tableData: [{
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '家',
+          age:20
         }, {
           date: '2016-05-04',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1517 弄',
+          tag: '公司',
+          age:22
         }, {
           date: '2016-05-01',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1519 弄',
+          tag: '家',
+          age:24
         }, {
           date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }],
-        dialogTableVisible: false,
-        dialogFormVisible: false,
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        },
-        formLabelWidth: '120px'
-      };
+          address: '上海市普陀区金沙江路 1516 弄',
+          tag: '公司'
+          age:25,
+        }]
+      }
+    },
+    methods: {
+      formatter(row, column) {
+        return row.address;
+      },
+      filterTag(value, row) {
+        return row.tag === value;
+      }
     }
-  };
+  }
 </script>
