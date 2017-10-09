792b1a1f8761005eb6fd2fd196966f5a5bf46126
kevin <kevin.e@qq.com>
2017-10-09 17:38:33 +0800

    mod footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -3,7 +3,7 @@
       <el-col :span="24">
           <div class="footer">
           <p>Copyright © 2017 深圳市云享智能科技有限公司 技术支持 0755-26525294</p>
-          <a href="http://www.miitbeian.gov.cn">粤ICP备09149236 号</a>
+          <a href="http://www.miitbeian.gov.cn">粤ICP备16054827号-1</a>
           </div>
       </el-col>
   </el-row>
